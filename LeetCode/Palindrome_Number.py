09fec65aa1bfe1407083117eb6d94d61572b61fc
shourove000 <49043946+shourove000@users.noreply.github.com>
2024-10-05 23:13:10 +0600

    Update Palindrome_Number.py

--- LeetCode/Palindrome_Number.py
+++ LeetCode/Palindrome_Number.py
@@ -1,8 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # y = [int(digit) for digit in str(x)] 
         x = list(str(x))
-        print(x)
         reversed_x = x[::-1]  
-        print(reversed_x)
         return x == reversed_x
+    
